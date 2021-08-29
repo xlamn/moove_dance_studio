879ef5f8ce06a4798d95842800dd0d059fa912c9
TuanLamNguyen <tuanlam.nguyen@partner.bmw.de>
2021-08-29 21:21:00 +0200

    add primarySwatches

--- lib/src/main.dart
+++ lib/src/main.dart
@@ -1,6 +1,38 @@
 import 'package:flutter/material.dart';
 import 'package:moove_dance_studio/moove_dance_studio.dart';
 
+const MaterialColor _primaryWhite = MaterialColor(
+  0xFFFFFFFF,
+  <int, Color>{
+    50: Color(0xFFFFFFFF),
+    100: Color(0xFFFFFFFF),
+    200: Color(0xFFFFFFFF),
+    300: Color(0xFFFFFFFF),
+    400: Color(0xFFFFFFFF),
+    500: Color(0xFFFFFFFF),
+    600: Color(0xFFFFFFFF),
+    700: Color(0xFFFFFFFF),
+    800: Color(0xFFFFFFFF),
+    900: Color(0xFFFFFFFF),
+  },
+);
+
+const MaterialColor _primaryBlack = MaterialColor(
+  0xFF000000,
+  <int, Color>{
+    50: Color(0xFF000000),
+    100: Color(0xFF000000),
+    200: Color(0xFF000000),
+    300: Color(0xFF000000),
+    400: Color(0xFF000000),
+    500: Color(0xFF000000),
+    600: Color(0xFF000000),
+    700: Color(0xFF000000),
+    800: Color(0xFF000000),
+    900: Color(0xFF000000),
+  },
+);
+
 void main() {
   runApp(MyApp());
 }
@@ -11,10 +43,13 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       theme: ThemeData(
         brightness: Brightness.light,
-        primaryColor: Colors.grey[50]!,
+        primarySwatch: _primaryBlack,
+        primaryColor: Colors.white,
       ),
       darkTheme: ThemeData(
+        primarySwatch: _primaryWhite,
         brightness: Brightness.dark,
+        primaryColor: Colors.grey[800]!,
       ),
       themeMode: ThemeMode.system,
       home: MyHomePage(),
